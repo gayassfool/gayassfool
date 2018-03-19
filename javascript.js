ce2b11cc3914667fddd2fc7e0ef578398a6cc6c4
gayassfool <37536912+gayassfool@users.noreply.github.com>
2018-03-19 10:17:18 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+window.alert("Start Loading JS");
+var MyWindow;
+
+function Virginitychecker()
+{
+	 Virginity=window.prompt("Voer je laatste scheikundecijfer in.", "")
+	 if(Virginity >= 7)
+	 {
+		 window.alert("Congrats nigga, you ass a mfing virgin bitch ass nigga.");
+	 }
+	 else
+		 
+		 {
+			 window.alert("Aight you good.");
+		 }	 
+}
+
+window.alert("JS Loaded");
